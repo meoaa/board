ffecd7bddf351555f1aed24224b859484b112a6d
meoaa <wsxqaz789456@naver.com>
2025-06-30 12:19:42 +0900

    Entity Graph

--- src/main/java/project/board/post/repository/PostRepository.java
+++ src/main/java/project/board/post/repository/PostRepository.java
@@ -1,5 +1,8 @@
 package project.board.post.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.board.post.domain.Post;
 
@@ -10,4 +13,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByTitleContaining(String keyword);
     List<Post> findByMember_UsernameContaining(String keyword);
 
+    @EntityGraph(attributePaths = {"member"})
+    Page<Post> findAll(Pageable pageable);
 }
